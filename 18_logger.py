ffeeb433b2acac869e6483532ed2019bf7fdca19
David Philiphose <dmpme@hotmail.com>
2018-03-24 14:59:43 -0500

    Update 18_logger.py

--- 18_logger.py
+++ 18_logger.py
@@ -51,21 +51,3 @@ while True:
 
 
 
-
-
-
-
-adxl345 = ADXL345()
-sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
-
-
-
-degrees = sensor.read_temperature()
-pascals = sensor.read_pressure()
-hectopascals = pascals / 100
-humidity = sensor.read_humidity()
-
-print 'Temp      = {0:0.3f} deg C'.format(degrees)
-print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-print 'Humidity  = {0:0.2f} %'.format(humidity)
-
