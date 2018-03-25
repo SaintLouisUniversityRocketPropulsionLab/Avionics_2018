f11a4ba9636d355c74d6ad16e1a8cc89ced953ce
jts599 <35665508+jts599@users.noreply.github.com>
2018-03-25 17:34:04 -0500

    Update 18_logger.py

--- 18_logger.py
+++ 18_logger.py
@@ -1,57 +1,71 @@
 from Adafruit_BME280 import *
-import time
+from time import *
 from adxl345 import ADXL345
 import datetime
 import gps
 
-
-adxl345 = ADXL345()
-
-axes = adxl345.getAxes(True)
-
-sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
-
+t = gmtime(time()-18000)
+fileName = str(t[0]) + "-" + str(t[1]) + "-" + str(t[2]) + "__" + str(t[3]) + "-" + str(t[4]) + "-" + str(t[5]) + ".csv"
+f = file(fileName, 'w')
+f.write("Time,Temp,Pressure,Humidity, Ax,Ay,Az, Longitue, Latitude, Altitude\n")
+f.close()
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-degrees = sensor.read_temperature()
-pascals = sensor.read_pressure()
-hectopascals = pascals / 100
-humidity = sensor.read_humidity()
+longitude = 0.0
+latitude = 0.0
+altitude = 0.0
+
+while True:
+    t = gmtime(time()-18000)
+    timestamp = strftime(" %d %b %Y %H:%M:%S")
+
+    adxl345 = ADXL345()
+
+    axes = adxl345.getAxes(True)
+
+    sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+    #does not require error checking because read temp will wait for new value internally
+    #other values will return the last read if no new val reported
+    
+    degrees = sensor.read_temperature()
+    pascals = sensor.read_pressure()
+    hectopascals = pascals / 100
+    humidity = sensor.read_humidity()
 
 
-
-print 'Temp      = {0:0.3f} deg C'.format(degrees)
-print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-print 'Humidity  = {0:0.2f} %'.format(humidity)
-
-print "   x = %.3fG" % ( axes['x'] )
-print "   y = %.3fG" % ( axes['y'] )
-print "   z = %.3fG" % ( axes['z'] )
+    x = "%.3fG" % ( axes['x'] )
+    y = "%.3fG" % ( axes['y'] )
+    z = "%.3fG" % ( axes['z'] )
 
 
-report = session.next()
-#print report
-if report['class'] == 'TPV':
-    if hasattr(report, 'time'):
-        print report.time
-    if hasattr(report, 'lon'):
-        print "longitude:"+str(report.lon)
-    if hasattr(report, 'lat'):
-        print "latitude:"+str(report.lat)
-    if hasattr(report, 'alt'):
-        print "altitude:"+st(report.alt)
-  except KeyError:
-    pass
-  except KeyboardInterrupt:
-    quit()
-  except StopIteration:
-    session = None
-    print "GPSD has terminated"
-
-
-
-
-
-
+    #If a value is missing will write an 'X' as value in csv file
+    
+    try:
+        report = session.next()
+        if report['class'] == 'TPV':
+            if hasattr(report, 'lon'):
+                longitude = report.lon
+            else:
+                longitude = 'X'
+            if hasattr(report, 'lat'):
+                latitude = report.lat
+            else:
+                latitude = 'X'
+            if hasattr(report, 'alt'):
+                altitude = report.alt
+            else:
+                altitude = 'X'
+    except KeyError:
+        pass
+    except StopIteration:
+        session = none
+        print "GPSD HAS ENDED"
+        
+    outString = str(timestamp)+","+str(degrees)+","+str(hectopascals)+","+str(humidity)+","+str(x)+"," + str(y)+","+str(z)+","+str(longitude)+","+str(latitude)+","+str(altitude) + "\n"
+    f = file(fileName, 'a')
+    f.write(outString)
+    f.close()
+    print "Reading Recorded"
 
