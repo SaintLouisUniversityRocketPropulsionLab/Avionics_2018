f77866b71d87d66939d1e480b05d036d3132d017
jkloser <john.kloser3@gmail.com>
2018-04-28 13:44:05 -0500

    Update cam_example.py

--- cam_example.py
+++ cam_example.py
@@ -3,7 +3,7 @@ from time import sleep
 camera=PiCamera()
 
 camera.start_preview()
-camera.start_recording('/home/pi/video.h264')
+camera.start_recording('/home/pi/Desktop/video.h264')
 sleep(10)
 camera.stop_recording()
 camera.stop_preview()
